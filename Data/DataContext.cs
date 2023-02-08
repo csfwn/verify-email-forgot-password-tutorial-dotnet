0e2f1ee9964662399d19b6235a051125ab36ad5b
csfwn <chesafwan1997@gmail.com>
2023-02-08 16:23:18 +0800

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿namespace VerifyEmailForgotPasswordTutorial.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=userdb;Trusted_Connection=True;");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
